05f1f963ff204b7957f443f890f2fe29462c9e9e
Lucas Pickering <lucas@lucaspickering.me>
2020-06-18 17:08:52 -0400

    bash: fix quotes on purge macro

--- bash/helpers/aliases.sh
+++ bash/helpers/aliases.sh
@@ -7,7 +7,7 @@ alias ls="ls --color=auto" # Show color
 alias grep="grep --color=auto"  # Show color
 alias cls="printf '\ec'"
 alias pyclean="fd -I __pycache__ -x rm -r; fd -I -e pyc -x rm"
-alias purge="sudo apt-get purge $(dpkg -l | grep '^rc' | awk '{print $2}')"
+alias purge='sudo apt-get purge $(dpkg -l | grep "^rc\" | awk "{print $2}")'
 alias g="git"
 alias d="docker"
 alias dc="docker-compose"
