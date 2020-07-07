8fccc9e4bc08ff3e96dbc51033c5ac0d7b232d56
Lucas Pickering <lucas@lucaspickering.me>
2020-07-07 08:48:29 -0400

    fish: remove fish_variables

--- fish/config.lpmbp.fish
+++ fish/config.lpmbp.fish
@@ -13,3 +13,28 @@ status --is-interactive; and pyenv init - | source
 status --is-interactive; and pyenv virtualenv-init - | source
 
 bass 'source ~/git/infrav3/aliases.sh'
+
+# function mysqlcreds
+#     set scope $argv[0]
+
+#     switch "$scope"
+#         case local
+#             set secret_id "arn:aws:secretsmanager:us-east-1:692674046581:secret:container_rds_credentials-hLp3Ja"
+#             set rgx ".docker_portal_user, .docker_portal_passwd"
+#         case remote
+#             set secret_id "arn:aws:secretsmanager:us-east-1:692674046581:secret:cp_rds_credentials-3bM21P"
+#             set rgx ".dev_rds_master_user, .dev_rds_master_pass"
+#         case prd
+#             set secret_id "arn:aws:secretsmanager:us-east-1:692674046581:secret:cp_rds_credentials-3bM21P"
+#             set rgx ".portal_user, .prod_portal_pass"
+#         case '*'
+#             echo "Invalid scope"
+#             return 1
+#     end
+
+#     mysql_creds (aws secretsmanager get-secret-value --secret-id $secret_id)
+#     read -d "\n" USER PASSWORD < (echo $mysql_creds | jq -r '.SecretString' | jq -r "$rgx")
+#     echo "User: $USER; Password: $PASSWORD"
+#     export AWS_DB_USER=$USER
+#     export AWS_DB_PASSWORD=$PASSWORD
+# end
