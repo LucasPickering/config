339a640ed3a040c0951dccb417c20a355f655aa8
Lucas Pickering <lucas@lucaspickering.me>
2021-01-06 11:27:06 -0500

    More MF setup, add pyenv to fish

---
+++ fish/config.MF-0819.fish
@@ -0,0 +1,3 @@
+alias j="jira"
+alias gomp="python ~/MarkForged/Toolbox/etc/gomp.py"
+alias nw="/Applications/nwjs.app/Contents/MacOS/nwjs"
--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@ if not functions -q fisher
     fish -c fisher
 end
 
+
 alias src="source ~/.config/fish/config.fish"
 alias grep="grep --color=auto"  # Show color
 alias cls="printf '\ec'"
@@ -30,12 +31,29 @@ switch (uname)
     case Darwin
         alias copy="pbcopy"
         alias paste="pbpaste"
-        set PATH "/usr/local/opt/coreutils/libexec/gnubin/" $PATH
+        set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 end
 
 if test -d ~/.cargo
     set PATH $HOME/.cargo/bin $PATH
 end
+
+# Get directory of this script, for usage later
+# This has to go AFTER overriding PATH with our coreutils version (for Mac)
+set FISH_DIR (dirname (readlink -m (status --current-filename)))
+
+# Set up Pyenv
+set -Ux PYENV_ROOT $FISH_DIR/pyenv
+set PATH $PYENV_ROOT/bin $PATH
+set -Ux PYENV_VIRTUALENV_DISABLE_PROMPT 1
+# Install pyenv-virtualenv if it isn't already
+pyenv virtualenv --help 2>&1 > /dev/null
+if test $status -ne 0
+    echo "Installing pyenv-virtualenv"
+    git clone https://github.com/pyenv/pyenv-virtualenv.git (pyenv root)/plugins/pyenv-virtualenv
+end
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
 
 # Load additional config based on hostname
 set host_config ~/.config/fish/config.(hostname).fish
