88852e859ffa515cfa938bc836caf6bd2787da56
Lucas Pickering <lucas@lucaspickering.me>
2022-11-02 12:04:31 -0400

    fish: some BS stuff

--- fish/config.RMUS-ENG-MBP-902.fish
+++ fish/config.RMUS-ENG-MBP-902.fish
@@ -1,8 +1,13 @@
 alias j="jira"
+alias kdev="zli connect developer@development --targetGroup developers"
 
 fish_add_path ~/.bskube/bin
 
+set -Ux SKIP_ESLINT_LOADER true
+
 function docker_login
-    set region "us-east-1"
-    aws ecr get-login-password --region "$region" | docker login -u AWS --password-stdin 692674046581.dkr.ecr."$region".amazonaws.com
+    set region $argv[1]
+    test -z $region; and set region "us-east-1"
+    aws ecr get-login-password --region "$region" |\
+        docker login -u AWS --password-stdin 692674046581.dkr.ecr."$region".amazonaws.com
 end
