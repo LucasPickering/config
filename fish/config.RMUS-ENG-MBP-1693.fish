bf878bd9a877362d2c2303c9132b65c01cd2626a
Lucas Pickering <lucas@lucaspickering.me>
2023-11-21 15:03:59 -0500

    [fish] Change pgforward a little bit

--- fish/config.RMUS-ENG-MBP-1693.fish
+++ fish/config.RMUS-ENG-MBP-1693.fish
@@ -19,18 +19,18 @@ function creds
     es set aws dev
 end
 
-function pgforward --description "Port-forward kube to a postgres pod"
-    set pg_pods (kubectl get pod -o name | sed 's@pod/@@' | grep postgres)
-    set pod $pg_pods[1]
-    if test (count $pg_pods) -gt 1
-        echo "Multiple `postgres` pods detected, using the first"
+function pgforward --description "Port-forward kube to a postgres service"
+    set pg_services (kubectl get service -o name |  grep postgres)
+    set service $pg_services[1]
+    if test (count $pg_services) -gt 1
+        echo "Multiple `postgres` services detected, using the first"
     end
     set external_port 5432
     set internal_port 5433
     set c_normal (set_color normal)
     set c_bold (set_color --bold red)
-    echo "Forwarding to pod $c_bold$pod$c_normal"
-    kubectl port-forward $pod $internal_port:$external_port
+    echo "Forwarding to $c_bold$service$c_normal"
+    kubectl port-forward $service $internal_port:$external_port
 end
 
 function docker_login
