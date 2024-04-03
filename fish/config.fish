7689883a1699aeec1353603c9125786c12260d9c
Lucas Pickering <lucas@lucaspickering.me>
2024-04-03 06:23:37 -0400

    [fish] Use env-select

--- fish/config.fish
+++ fish/config.fish
@@ -70,3 +70,4 @@ end
 # Load additional config based on hostname
 set host_config ~/.config/fish/config.(hostname_base).fish
 test -r $host_config; and source $host_config
+es --shell fish init | source
