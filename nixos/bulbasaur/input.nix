2114ec2ecb8002294e3517a435d5d656c7f0326f
Lucas Pickering <lucas@lucaspickering.me>
2026-02-10 06:17:54 -0500

    nixos: keyd cmd layer

--- nixos/bulbasaur/input.nix
+++ nixos/bulbasaur/input.nix
@@ -9,6 +9,24 @@
         settings = {
           main = {
             capslock = "esc";
+            # Cmd key triggers the cmd layer for MacOS bindings
+            # keyd doesn't respect the alt<->meta swap from the DE,
+            # so this is on alt instead
+            # leftalt = "layer(cmd)";
+            rightalt = "layer(cmd)";
+          };
+
+          "cmd:C" = {
+            a = "C-a"; # Select all
+            c = "C-c"; # Copy
+            v = "C-v"; # Paste
+            x = "C-x"; # Cut
+            z = "C-z"; # Undo
+            s = "C-s"; # Save
+            f = "C-f"; # Find
+            w = "C-w"; # Close tab
+            t = "C-t"; # New tab
+            q = "C-q"; # Quit
           };
         };
       };
